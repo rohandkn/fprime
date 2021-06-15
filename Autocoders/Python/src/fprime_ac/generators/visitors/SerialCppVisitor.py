f5869526c388540a71c31e5d5b450ed894b07bff
Rohan Dhesikan <rohandhesikan@gmail.com>
2021-06-15 13:35:10 -0500

    Changed comment for visitor function

--- Autocoders/Python/src/fprime_ac/generators/visitors/SerialCppVisitor.py
+++ Autocoders/Python/src/fprime_ac/generators/visitors/SerialCppVisitor.py
@@ -104,9 +104,8 @@ class SerialCppVisitor(AbstractVisitor.AbstractVisitor):
 
     def _get_args_string(self, obj, prefix=""):
         """
-        Return a string of (type, name) args, comma separated
-        with a single element type for arrays
-        for use in templates that generate prototypes.
+        Return a string of name args, comma separated
+        for use in templates that generate method or function calls.
         """
         args = obj.get_members()
         arg_str = ""
